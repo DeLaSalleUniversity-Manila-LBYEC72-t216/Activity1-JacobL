23395d3a6b46efac152bbe696267570e881146df
lanzjacob <lanzharveyjacob@gmail.com>
2016-05-26 16:43:52 +0800

    Create Seatwork3.c

---
+++ Seatwork3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) 
+{
+	int year, age;
+	
+	printf("Enter your birth year: ");
+	scanf("%d", &year);
+	
+	age=2016-year;
+	
+	printf("You are %d years old.", age);
+	
+	return 0;
+}
