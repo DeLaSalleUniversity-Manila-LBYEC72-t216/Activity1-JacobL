3a16178d220ce81692beb95062e8aa4188bbeee8
lanzjacob <lanzharveyjacob@gmail.com>
2016-05-26 16:42:57 +0800

    Create Seatwork2.c

---
+++ Seatwork2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) 
+{
+	int base, height, area;
+	
+	printf("Enter the base of the triangle: ");
+	scanf("%d", &base);
+	printf("\n");
+	
+	printf("Enter the height of the triangle: ");
+	scanf("%d", &height);
+	printf("\n");
+	
+	area=base*height;
+	printf("The area of the triangle is %d", area);
+	printf("\n");
+	
+	return 0;
+}
